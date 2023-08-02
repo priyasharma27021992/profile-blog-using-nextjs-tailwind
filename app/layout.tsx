d17e8c87353e2497b51edd69dd4253ce90036908
priyasharma27021992 <priyasharma2702@gmail.com>
2023-08-02 18:52:37 +0530

    added initial files for project

---
+++ app/layout.tsx
@@ -0,0 +1,28 @@
+import "./globals.css";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import Navbar from "./components/Navbar";
+import MyProfilePic from "./components/MyProfilePic";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Priya's blog",
+  description: "Created by Priya - Miss Techie in Progress",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <Navbar />
+        <MyProfilePic />
+        {children}
+      </body>
+    </html>
+  );
+}
